69f872e2108c36538b3b82f1efd9a145983e9a2b
Tomasz Jaholkowski <jahol@ais.pl>
2016-10-10 22:10:57 +0200

    nano obj

--- GameEngine/Display.cpp
+++ GameEngine/Display.cpp
@@ -14,7 +14,7 @@ Display::Display(int width, int height, const std::string& title) : _width(width
 	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
 	window = SDL_CreateWindow(title.c_str(),
 		SDL_WINDOWPOS_CENTERED,
